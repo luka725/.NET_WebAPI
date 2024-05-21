e06cb264d3449a8a2a3d126a5274b84b3b49e057
luka <luka.sheylashvili@gmail.com>
2024-05-21 20:49:42 +0400

    implementing changes

--- WebApplication/App_Start/WebApiConfig.cs
+++ WebApplication/App_Start/WebApiConfig.cs
@@ -10,9 +10,6 @@ namespace WebApplication
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
             config.Filters.Add(new BasicAuthentication());
             config.Routes.MapHttpRoute(
