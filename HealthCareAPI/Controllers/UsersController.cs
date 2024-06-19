0d3946234f018585a51773c97a990c62a076fce2
luka <luka.sheylashvili@gmail.com>
2024-06-20 01:48:27 +0400

    adding cors to the project

--- HealthCareAPI/Controllers/UsersController.cs
+++ HealthCareAPI/Controllers/UsersController.cs
@@ -8,9 +8,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace HealthCareAPI.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     [RoutePrefix("api/users")]
     public class UsersController : ApiController
     {
